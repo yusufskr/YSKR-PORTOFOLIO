22ec8d1de4a806735fa7576db9f9120f647f48b0
yusufskr <120576081+yusufskr@users.noreply.github.com>
2025-09-18 10:00:30 +0200

    Create LoadingDots.jsx

---
+++ src/components/LoadingDots.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './LoadingDots.css';
+
+const LoadingDots = () => {
+  return (
+    <div className="loading-dots">
+      <div className="dot"></div>
+      <div className="dot"></div>
+      <div className="dot"></div>
+    </div>
+  );
+};
+
+export default LoadingDots; 
